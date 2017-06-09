a7fe65aeb07a54faa9c2cea7d21735bb04689062
Zachary Ehren <zachehren@gmail.com>
2017-06-08 20:34:18 -0700

    checkpoint submission complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -62,8 +62,8 @@ class Kele
       response = self.class.post("#{@base_url}/messages", headers: { "authorization" => @auth_token }, body: { sender: @email, recipient_id: recipient_id, token: token, subject: subject, 'stripped-text' => stripped_text } )
     end
 
-    def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
-      response = self.class.post("#{@base_url}/checkpoint_submissions", headers: { "authorization" => @auth_token }, body: { checkpoint_id: checkpoint_id, assignment_branch: assignment_branch, assignment_commit_link: assignment_commit_link, 'comment' => comment } )
+    def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+      response = self.class.post("#{@base_url}/checkpoint_submissions", headers: { "authorization" => @auth_token }, body: { checkpoint_id: checkpoint_id, assignment_branch: assignment_branch, assignment_commit_link: assignment_commit_link, enrollment_id: enrollment_id, 'comment' => comment } )
     end
 
 private
